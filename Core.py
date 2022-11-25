f13588c94cad7f6ae3b09cd8e1e0f249c99ca78b
First Kayl <vla.ka@mail.ru>
2022-11-25 09:48:41 +0300

    Start

---
+++ Core.py
@@ -0,0 +1,21 @@
+import pygame as pg
+
+
+
+def main():
+    pg.init()
+
+    #while running:
+    #    pass
+
+def render():
+    pass
+
+def update():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
