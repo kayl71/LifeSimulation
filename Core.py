a144d8c9a5e4eae2f4c8fc9e315acff449b011ac
First Kayl <vla.ka@mail.ru>
2022-11-25 10:32:02 +0300

    Start

--- Core.py
+++ Core.py
@@ -5,7 +5,30 @@ running = False
 class Core:
 
     def __init__(self):
+        self.creatures = []
+        self.alive = True
         pg.init()
+        width = 800
+        height = 800
+        self.screen = pg.display.set_mode((width, height))
+
+    def run(self):
+
+        self.start()
+        time_now = pg.time.get_ticks()
+        time_last_update = time_now
+        time_last_render = time_now
+        UPS = 4
+        FPS = 60
+
+        while self.alive:
+            if self.running and time_now - time_last_update > 1000 / UPS:
+                self.update()
+            if time_now - time_last_render > 1000 / FPS:
+                self.render()
+
+
+
 
     def render(self):
         pass
@@ -16,10 +39,13 @@ class Core:
     def start(self):
         self.running = True
 
+    def stop(self):
+        self.running = False
+
 
 
 
 
 if __name__ == "__main__":
     core = Core()
-    core.start()
+    core.run()
